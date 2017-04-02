8f412da079327253551559419a549e5b57eddcf8
NaokiCho <naoki.cho7@gmail.com>
2017-04-02 18:28:53 -0400

    Mag + Gyro data fusion for heading

--- HMC5883L.h
+++ HMC5883L.h
@@ -1,16 +1,29 @@
 #include <Wire.h> //I2C Arduino Library
+#include <EEPROM.h>
 
 #define address 0x1E //0011110b, I2C 7bit address of HMC5883
 
-void initmag(){
+short x_offset, y_offset;
+float scale;
+
+void initmag() {
   Wire.begin();
   Wire.beginTransmission(address); //open communication with HMC5883
   Wire.write(0x02); //select mode register
   Wire.write(0x00); //continuous measurement mode
   Wire.endTransmission();
+  
+  short x_max = EEPROM.read(0) | (EEPROM.read(1)<<8);
+  short x_min = EEPROM.read(2) | (EEPROM.read(3)<<8);
+  short y_max = EEPROM.read(4) | (EEPROM.read(5)<<8);
+  short y_min = EEPROM.read(6) | (EEPROM.read(7)<<8);
+  
+  x_offset = (x_max + x_min)/2;
+  y_offset = (y_max + y_min)/2;
+  scale = ((float)(x_max-x_offset))/((float)(y_max-y_offset));
 }
 
-void getMagData(short buf[]){
+void getMagData(short buf[]) {
   Wire.beginTransmission(address);
   Wire.write(0x03); //select register 3, X MSB register
   Wire.endTransmission();
@@ -26,3 +39,47 @@ void getMagData(short buf[]){
   }
 }
 
+void getFixedMagData(short buf[]) {
+  getMagData(buf);
+  buf[0] -= x_offset;
+  buf[1] -= y_offset;
+//  buf[1] *= scale;
+}
+
+float getHeading() {
+  short buf[3];
+  getFixedMagData(buf);
+  return atan2((float)buf[1],(float)buf[0])/3.14159265*180.0;
+}
+
+void magCalibrate() {
+  pinMode(6,INPUT_PULLUP);
+  if(digitalRead(6))
+    return;
+  short x_max, x_min, y_max, y_min;
+  short magVals[3];
+  getMagData(magVals);
+  x_max = magVals[0];
+  x_min = magVals[0];
+  y_max = magVals[1];
+  y_min = magVals[1];
+  unsigned long timer = millis() + 10000;
+  while(millis() < timer) {
+    getMagData(magVals);
+    x_max = max(x_max,magVals[0]);
+    x_min = min(x_min,magVals[0]);
+    y_max = max(y_max,magVals[1]);
+    y_min = min(y_min,magVals[1]);
+  }
+  EEPROM.write(0,x_max & 0xFF);
+  EEPROM.write(1,x_max >> 8);
+  EEPROM.write(2,x_min & 0xFF);
+  EEPROM.write(3,x_min >> 8);
+  EEPROM.write(4,y_max & 0xFF);
+  EEPROM.write(5,y_max >> 8);
+  EEPROM.write(6,y_min & 0xFF);
+  EEPROM.write(7,y_min >> 8);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  while(1);
+}
--- Parking_v2.ino
+++ Parking_v2.ino
@@ -1,6 +1,7 @@
 #include "ITG3200.h"
 #include "HMC5883L.h"
 #include "L298N.h"
+#include "NewPing.h"
 #include <Servo.h>
 
 // Left Motor
@@ -19,30 +20,71 @@ Servo front;
 #define FLATVAL_B 49
 Servo back;
 // Front Sonar
-//#define FSONAR 10
+#define FSONAR_TRIG 22
+#define FSONAR_ECHO 21
+#define MAX_DISTANCE 200
+NewPing fsonar(FSONAR_TRIG,FSONAR_ECHO,MAX_DISTANCE);
 // Back Sonar
-//#define BSONAR 15
+#define BSONAR_TRIG 16
+#define BSONAR_ECHO 17
+NewPing bsonar(BSONAR_TRIG,BSONAR_ECHO,MAX_DISTANCE);
+// Left Encoder
+#define LENCODER 3  
+// Right Encoder
+#define RENCODER 2
+
+volatile unsigned int lpulse, rpulse;
+volatile unsigned long ltimer, rtimer;
+
+float heading,dps,prevdps,init_heading;                                                
+unsigned long timer;
+int fangle, bangle;
+
+void risingTrig_l() {
+  ltimer = millis();
+  attachInterrupt(LENCODER, fallingTrig_l, FALLING);
+}
+
+void fallingTrig_l() {
+  lpulse = millis() - ltimer;
+  attachInterrupt(LENCODER, risingTrig_l, RISING);
+}
+
+void risingTrig_r() {
+  rtimer = millis();
+  attachInterrupt(RENCODER, fallingTrig_r, FALLING);
+}
+
+void fallingTrig_r() {
+  rpulse = millis() - rtimer;
+  attachInterrupt(RENCODER, risingTrig_r, RISING);
+}
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+  attachInterrupt(LENCODER, risingTrig_l, RISING);
+  attachInterrupt(RENCODER, risingTrig_r, RISING);
   // Reset ITG3200
   itgWrite(itgAddress,0x3E,0x80);
   //Set the gyroscope scale for the outputs to +/-2000 degrees per second
   itgWrite(itgAddress, DLPF_FS, (DLPF_FS_SEL_0|DLPF_FS_SEL_1|DLPF_CFG_0));
   //Set the sample rate to 100 hz
   itgWrite(itgAddress, SMPLRT_DIV, 9);
-  counteroffset();
-
-//  left.assignPins(OUT2,OUT1);
-//  right.assignPins(OUT4,OUT3);
+  initmag();
+  magCalibrate();
+  left.assignPins(OUT2,OUT1);
+  right.assignPins(OUT4,OUT3);
   front.attach(FSERVO);
   back.attach(BSERVO);
+  heading = getHeading();
+  init_heading = getHeading();
+//  left.move(255);
+//  right.move(255);
+//  while(1) {
+//    Serial.println(getHeading());
+//  }
 }
-
-float drh,dps,prevdps;                                                    
-unsigned long timer;
-int fangle, bangle;
 
 void loop() {
   unsigned long dt = micros()-timer;
@@ -50,19 +92,21 @@ void loop() {
   float prevdps = dps;
   dps = (float)readZ() / 14.375;
   float trapezoid = (prevdps+dps)*(float)dt*0.0000005;
-  drh+=trapezoid;
+  heading = (heading-trapezoid)*0.98+getHeading()*0.02;
+  ctrlFrontServo(0);
+  ctrlBackServo(0);
 }
 
 // front > = CCW
 // back > = CCW
 // CW = <
 void ctrlFrontServo(int goal) {
-  fangle = FLATVAL_F - drh - goal;
+  fangle = FLATVAL_F + heading - goal - init_heading;
   front.write(fangle);
 }
 
 void ctrlBackServo(int goal) {
-  bangle = FLATVAL_B - drh - goal;
+  bangle = FLATVAL_B + heading - goal - init_heading;
   back.write(bangle);
 }
 
